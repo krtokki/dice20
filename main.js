e1990d63159abbace65ba00261e8e0b6202cc5ec
krtokki <krtokkival@gmail.com>
2026-04-08 04:17:56 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 5000 );
 
 const light = new THREE.AmbientLight(0xffffff, 1); 
 scene.add(light);
+scene.add(new THREE.GridHelper(20, 20));
+scene.add(new THREE.AxesHelper(10));
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(1);
