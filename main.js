e119ebb4fab2f31f00f77e2b8889a1fcc8acea54
krtokki <krtokkival@gmail.com>
2026-04-08 04:11:18 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
