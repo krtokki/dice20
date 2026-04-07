55c94f0206fde3a67e0bae84cdb36ed5e43a9981
krtokki <krtokkival@gmail.com>
2026-04-07 21:42:10 +0900

    changed camera rebound speed

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ const originalTarget = new THREE.Vector3(0, 0, 0);
 const currentSpherical = new THREE.Spherical();
 const targetSpherical = new THREE.Spherical().setFromVector3(originalPosition);
 
-const reboundSpeed = 0.0002;
+const reboundSpeed = 0.0003;
 let isInteracting = false;
 
 camera.position.copy(originalPosition);
