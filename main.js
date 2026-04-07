344f6330c240e8d3270f5d322fae8ac7f63d17db
krtokki <krtokkival@gmail.com>
2026-04-07 23:42:55 +0900

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ document.body.appendChild( renderer.domElement );
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true; 
-controls.dampingFactor = 0.05;
+controls.dampingFactor = 0.15;
 
 const loader = new GLTFLoader();
 let model;
