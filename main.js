db69a6afbf714f3a8240af8ce8281ca5e588ec38
krtokki <krtokkival@gmail.com>
2026-04-08 01:54:26 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setPixelRatio(0.13);
+renderer.setPixelRatio(0.5);
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
