45623026c520dd5624025f70223eb2b56d583e48
krtokki <krtokkival@gmail.com>
2026-04-07 20:29:48 +0900

    update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const controls = new OrbitControls(camera, renderer.domElement);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -15,11 +16,11 @@ scene.add( cube );
 
 camera.position.z = 5;
 
-function animate( time ) {
+controls.enableDamping = true; 
 
-  cube.rotation.x = time / 2000;
-  cube.rotation.y = time / 1000;
-
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
   renderer.render( scene, camera );
-
 }
+renderer.setAnimationLoop( animate );
