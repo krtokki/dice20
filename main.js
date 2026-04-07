7a3b9b09130481348380e9184fbdc74a9cf03cbc
krtokki <krtokkival@gmail.com>
2026-04-08 04:09:09 +0900

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+const light = new THREE.AmbientLight(0xffffff, 1); 
+scene.add(light);
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(1);
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -22,6 +25,5 @@ loader.load('models/table.glb', (gltf) => {
 camera.position.z = 5;
 
 function animate() {
-  controls.update();
   renderer.render( scene, camera );
 }
