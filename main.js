96fe43aac0e8d2a53cba1715f21e887d13d92958
krtokki <krtokkival@gmail.com>
2026-04-08 12:30:32 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,13 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+let table;
+
 const loader = new GLTFLoader();
 loader.load( 'models/table.glb', function ( gltf ) {
-  scene.add( gltf.scene );
+  const table = gltf.scene;
+  table.scale.setScalar(0.5);
+  scene.add( table );
 });
 
 const light = new THREE.AmbientLight( 0x404040, 1 );
