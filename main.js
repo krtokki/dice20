51b48178ac73ddae733258ae099c5c60c4fc6ebf
krtokki <krtokkival@gmail.com>
2026-04-08 12:23:00 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const loader = new GLTFLoader();
+loader.load( 'models/table.glb', function ( gltf ) {
+  scene.add( gltf.scene );
+});
+
+const light = new THREE.AmbientLight( 0x404040, 1 );
+scene.add( light );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(1);
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
 camera.position.set = (0, 0, 5);
 
