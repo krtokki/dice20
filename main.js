a839f0722c785101de58eb2998c02956ed469b49
krtokki <krtokkival@gmail.com>
2026-04-08 12:12:08 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const light = new THREE.AmbientLight(0xffffff, 1); 
-scene.add(light);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(1);
@@ -14,24 +9,12 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const controls = new OrbitControls(camera, renderer.domElement);
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-const loader = new GLTFLoader();
-let table;
-
-loader.load('models/table.glb', (gltf) => {
-  table = gltf.scene;
-  const box = new THREE.Box3().setFromObject(table);
-  const center = box.getCenter(new THREE.Vector3());
-  table.position.sub(center);
-  table.scale.setScalar(0.5);
-  scene.add(table);
-
-  controls.target.set(2, 0, 0);
-  controls.update;
-});
-
-camera.position.set = (2, 0, 5);
+camera.position.set = (0, 0, 5);
 
 function animate() {
   renderer.render( scene, camera );
