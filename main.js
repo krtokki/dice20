b482b0188c7403970b760be642ca1001b0e50d95
krtokki <krtokkival@gmail.com>
2026-04-10 01:43:04 +0900

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ async function initPhysics() {
 
 function createDebugFloor() {
   const width = 1.8;
-  const height = 0.3;
+  const height = 0.1;
   const depth = 1.25;
 
   let bodyDesc = RAPIER.RigidBodyDesc.fixed().setTranslation(0, 0.3, 0);
