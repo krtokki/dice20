80b6f118f2074dd4813018bb9711be57ecb5b2c9
krtokki <krtokkival@gmail.com>
2026-04-13 20:05:03 +0900

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ tablelightone.position.set(5, 5, 0);
 tablelightone.lookAt(0, 0, 0);
 tablelightone.layers.set(1);
 scene.add( tablelightone );
-const tablelighttwo = new THREE.AmbientLight( 0xffffff, 1.5 );
+const tablelighttwo = new THREE.AmbientLight( 0xffffff, 2.5 );
 tablelighttwo.layers.set(1);
 scene.add( tablelighttwo );
 
